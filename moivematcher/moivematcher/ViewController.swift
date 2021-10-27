d15c0da9a5a3152716cb011ab1caaf6a4e09734b
Zhe Wang <zhe.wang1998@duke.edu>
2021-10-27 11:07:25 -0400

    add initial UI layout

---
+++ moivematcher/moivematcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moivematcher
+//
+//  Created by Zhe Wang on 2021-10-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
