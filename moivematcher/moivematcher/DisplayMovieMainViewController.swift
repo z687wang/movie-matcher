fbb673a081a14d217b1434241cc640083236acdb
Zhe Wang <zhe.wang1998@duke.edu>
2021-11-08 00:31:17 -0500

    add controller for displaying active movies and change movie fetch api implementation

--- moivematcher/moivematcher/DisplayMovieMainViewController.swift
+++ moivematcher/moivematcher/DisplayMovieMainViewController.swift
@@ -6,37 +6,170 @@
 //
 
 import UIKit
+import SwiftUI
+import Combine
+
+
+var movieIDArray: [Int] = []
+var page: Int = 1
+var hasNextPage: Bool = true
+
+class ActiveMoviesModel: ObservableObject {
+    var didChange = PassthroughSubject<Void, Never>()
+    var activeMovies: [MovieWithGenres] { didSet { didChange.send() } }
+    init(activeMovies: [MovieWithGenres]) {
+            self.activeMovies = activeMovies
+        }
+}
+
+var activeMoviesModel = ActiveMoviesModel(activeMovies: [])
+
+struct MoviesSectionView: View {
+    @ObservedObject var moviesModel: ActiveMoviesModel
+    var body: some View {
+        ZStack{
+            ForEach(moviesModel.activeMovies) { movie in
+                MoviePosterView(movie: movie)
+            }
+        }
+        .padding(8)
+        .zIndex(1.0)
+    }
+}
+
+class MoviesSectionSwiftUIViewHostingController: UIHostingController<MoviesSectionView> {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder, rootView: MoviesSectionView(moviesModel: activeMoviesModel))
+    }
+    func setNeedsBodyUpdate() {
+        
+    }
+}
+
+struct MoviePosterView: View {
+    @State var movie: MovieWithGenres
+    let movieGradient = Gradient(colors: [Color.black.opacity(0.0), Color.black.opacity(0.5)])
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            AsyncImage(url: URL(string: movie.poster_path!));
+                
+            // Linear Gradient
+            LinearGradient(gradient: movieGradient, startPoint: .top, endPoint: .bottom)
+            VStack {
+                Spacer()
+                VStack(alignment: .leading){
+                    HStack {
+                        Text(movie.title).font(.largeTitle).fontWeight(.bold)
+                        Text(String(movie.releaseDate!)).font(.title)
+                    }
+                }
+            }
+            .padding()
+            .foregroundColor(.white)
+        }
+        
+        .cornerRadius(8)
+        .offset(x: movie.x, y: movie.y)
+        .rotationEffect(.init(degrees: movie.degree))
+        .gesture (
+            DragGesture()
+                .onChanged { value in
+                    withAnimation(.default) {
+                        movie.x = value.translation.width
+                        // MARK: - BUG 5
+                        movie.y = value.translation.height
+                        movie.degree = 7 * (value.translation.width > 0 ? 1 : -1)
+                    }
+                }
+                .onEnded { (value) in
+                    withAnimation(.interpolatingSpring(mass: 1.0, stiffness: 50, damping: 8, initialVelocity: 0)) {
+                        switch value.translation.width {
+                        case 0...100:
+                            movie.x = 0; movie.degree = 0; movie.y = 0
+                        case let x where x > 100:
+                            movie.x = 500; movie.degree = 12
+                        case (-100)...(-1):
+                            movie.x = 0; movie.degree = 0; movie.y = 0
+                        case let x where x < -100:
+                            movie.x  = -500; movie.degree = -12
+                        default:
+                            movie.x = 0; movie.y = 0
+                        }
+                    }
+                }
+        )
+    }
+}
+
 
 class DisplayMovieMainViewController: UIViewController {
 
-    @IBOutlet weak var ShowMovieDetailButton: UIButton!
     @IBOutlet weak var MovieNameLabel: UILabel!
     @IBOutlet weak var MovieYearLabel: UILabel!
-    @IBOutlet weak var DislikeButton: UIButton!
-    @IBOutlet weak var LikeButton: UIButton!
-    var currentMovie: Int!
-    public var moviesArray: [Int] = [];
+    @IBOutlet weak var MoviesView: UIView!
+    
+    var apiClient = MovieApiClient()
+    
     override func viewDidLoad() {
-        print("DisplayMovieMainView Loaded")
-        super.viewDidLoad()
-        self.currentMovie = 0;
+        super.viewDidLoad();
+        loadMoviesIDData();
     }
     
-    @IBAction func DislikeButtonClick(_ sender: UIButton) {
+    func loadMoviesIDData() {
+        print("start to load data")
+        fetchInitialMoviesID(with: page)
     }
     
-    @IBAction func LikeButtonClick(_ sender: UIButton) {
+    func fetchInitialMoviesID(with page: Int) {
+        self.apiClient.fetchMoviesID(page: page) { [weak self] (results) in
+            switch results {
+            case .failure(let error):
+                print(error)
+            case .success(let resource, let hasPage):
+                movieIDArray = resource
+                hasNextPage = hasPage
+                self?.fetchGroupMoviesDetails(from: movieIDArray, completionHandler: { movies in
+                    print(movies)
+                })
+            }
+            
+        }
+        
     }
     
-    
-    func updateViewWithNextMovie() {
-        self.currentMovie += 1;
+    func fetchMovieDetails(from id: Int, completionHandler: @escaping (_ movie: Movie)-> Void) {
+        let group = DispatchGroup()
+        group.enter()
+        self.apiClient.fetchMovieDetails(movieId: String(id), completion:{ (result) in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(let resource , _):
+                activeMoviesModel.activeMovies.append(resource)
+            }
+            group.leave()
+        })
     }
     
-    func fetchNewMovie(currentMovie: Int) {
-        var targetMovieId: Int = moviesArray[self.currentMovie];
-        var targetMovie = fetchMovieData(movieId: targetMovieId);
-        return;
+    func fetchGroupMoviesDetails(from moviesId: [Int], completionHandler: @escaping (_ movies: [MovieWithGenres])-> Void) {
+        let group = DispatchGroup()
+        for id in moviesId {
+            group.enter()
+            self.apiClient.fetchMovieDetails(movieId: String(id), completion:{ (result) in
+                switch result {
+                case .failure(let error):
+                    print(error)
+                case .success(let resource , _):
+                    activeMoviesModel.activeMovies.append(resource)
+                }
+                group.leave()
+            })
+        }
+        
+//        group.notify(queue: DispatchQueue.global(qos: .userInitiated)) {
+//            completionHandler(movies)
+//        }
     }
     
     /*
--- moivematcher/moivematcher/MovieDatabaseApi.swift
+++ moivematcher/moivematcher/MovieDatabaseApi.swift
@@ -1,9 +1,3 @@
-//
-//  File.swift
-//  moivematcher
-//
-//  Created by Yalu Cai on 11/1/21.
-//
 
 import Foundation
 
@@ -34,14 +28,13 @@ extension Endpoint {
     }
 }
 
-enum MovieNightEndpoint: Endpoint {
+enum MovieEndpoint: Endpoint {
     case Genre(page: String)
     case Actor(page: String)
     case Movie(page: String)
+    case MovieDetails(id: String)
     case MovieRecommendations(id: String)
     case MovieCredits(id: String)
-    case MoviePoster(id: String)
-    case MovieVideos(id: String)
     
     var baseURL: String {
         return "https://api.themoviedb.org"
@@ -56,12 +49,10 @@ enum MovieNightEndpoint: Endpoint {
                 return "/3/movie/popular"
             case .MovieRecommendations(let id):
                 return "/3/movie/\(id)/recommendations"
+            case .MovieDetails(let id):
+                return "/3/movie/\(id)"
             case .MovieCredits(let id):
                 return "/3/movie/\(id)/credits"
-            case .MoviePoster(let id):
-                return "/3/movie/\(id)/images"
-            case .MovieVideos(let id):
-                return "/3/movie/\(id)/videos"
         }
     }
     
@@ -73,16 +64,13 @@ enum MovieNightEndpoint: Endpoint {
         case .Actor(let page), .Movie(let page), .Genre(let page):
             parameters["page"] = page
             return parameters
-        case .MovieRecommendations, .MovieCredits, .MovieVideos:
-            return parameters
-        case .MoviePoster:
+        case .MovieDetails, .MovieRecommendations, .MovieCredits:
             return parameters
         }
     }
 }
 
-final class MovieNightApiClient: ApiClient, HttpClient {
-    
+final class MovieApiClient: ApiClient, HttpClient {
     var configuration: URLSessionConfiguration
     lazy var session: URLSession = {
         return URLSession(configuration: self.configuration)
@@ -96,18 +84,54 @@ final class MovieNightApiClient: ApiClient, HttpClient {
         self.init(configuration: .default)
     }
     
-    // Fetch popular movies
-    func fetchMovies(page: Int, completion: @escaping (APIResult<[Movie]>) -> Void) {
-        let endpoint = MovieNightEndpoint.Movie(page: String(page))
+    func fetchGenres(page: Int, completion: @escaping (APIResult<[Genre]>) -> Void) {
+        let endpoint = MovieEndpoint.Genre(page: String(page))
         let request = endpoint.request
-//        print(request.url)
+        
+        fetch(request: request, parse: { (json) -> [Genre]? in
+            guard let genres = json["genres"] as? [[String:AnyObject]] else {
+                return nil
+            }
+            return genres.flatMap {
+                do {
+                    return try Genre(JSON: $0)
+                } catch (let error){
+                    print(error)
+                }
+                return nil
+            }
+            
+        }, completion: completion)
+    }
+    
+    func fetchActors(page: Int, completion: @escaping (APIResult<[Actor]>) -> Void) {
+        let endpoint = MovieEndpoint.Actor(page: String(page))
+        let request = endpoint.request
+        
+        fetch(request: request, parse: { (json) -> [Actor]? in
+            guard let popularActors = json["results"] as? [[String:AnyObject]] else {
+                return nil
+            }
+            return popularActors.flatMap {
+                do {
+                    return try Actor(JSON: $0)
+                } catch (let error){
+                    print(error)
+                }
+                return nil
+            }
+        }, completion: completion)
+    }
+    
+    func fetchMovies(page: Int, completion: @escaping (APIResult<[Movie]>) -> Void) {
+        let endpoint = MovieEndpoint.Movie(page: String(page))
+        let request = endpoint.request
         
         fetch(request: request, parse: { (json) -> [Movie]? in
-//            print(json)
             guard let popularMovies = json["results"] as? [[String:AnyObject]] else {
                 return nil
             }
-            let movies = popularMovies.compactMap { (movie) -> Movie? in
+            let movies = popularMovies.flatMap { (movie) -> Movie? in
                 do {
                     return try Movie(JSON: movie)
                 } catch (let error){
@@ -124,117 +148,97 @@ final class MovieNightApiClient: ApiClient, HttpClient {
             }
         }, completion: completion)
     }
-        
-//    Fecth leading actors of the movie
-    func fetchMovieActors(movieId: String, completion: @escaping (APIResult<[PersonOfMovie]>) -> Void) {
-        
-        let endpoint = MovieNightEndpoint.MovieCredits(id: movieId)
+    
+    func fetchMoviesID(page: Int, completion: @escaping (APIResult<[Int]>) -> Void) {
+        let endpoint = MovieEndpoint.Movie(page: String(page))
         let request = endpoint.request
         
-        fetch(request: request, parse: { (json) -> [PersonOfMovie]? in
-            print(json["cast"])
-            guard let movieCredits = json["cast"] as? [[String:AnyObject]], let movieId = json["id"] as? Int else {
+        fetch(request: request, parse: { (json) -> [Int]? in
+            guard let popularMovies = json["results"] as? [[String:AnyObject]] else {
                 return nil
             }
-            
-            let actors = movieCredits.compactMap { (acteur) -> PersonOfMovie? in
+            let movies = popularMovies.flatMap { (movie) -> Int? in
                 do {
-                    return try PersonOfMovie(JSON: acteur, movieId: movieId)
+                    return try Movie(JSON: movie)?.hashValue
                 } catch (let error){
                     print(error)
                 }
                 return nil
             }
             
-            if actors.count < 5 {
-                return actors
+            
+            if movies.isEmpty {
+                return nil
+            } else {
+                return movies
             }
-            else {
-                return Array(actors.prefix(upTo: 5))
+        }, completion: completion)
+    }
+    
+    func fetchMovieDetails(movieId: String, completion: @escaping (APIResult<MovieWithGenres>) -> Void) {
+        let endpoint = MovieEndpoint.MovieDetails(id: movieId)
+        let request = endpoint.request
+        fetch(request: request, parse: { (json) -> MovieWithGenres? in
+            do {
+                return try MovieWithGenres(JSON: json)
+            } catch (let error){
+                print(error)
+            }
+            return nil
+        }, completion: completion)
+    }
+    
+    func fetchMoviesRecommendations(movieId: String, completion: @escaping (APIResult<[Movie]>) -> Void) {
+        
+        let endpoint = MovieEndpoint.MovieRecommendations(id: movieId)
+        let request =  endpoint.request
+        
+        fetch(request: request, parse: { (json) -> [Movie]? in
+            guard let popularMovies = json["results"] as? [[String:AnyObject]] else {
+                return nil
             }
             
+            let movies =  popularMovies.flatMap { (movie) -> Movie? in
+                do {
+                    return try Movie(JSON: movie)
+                } catch (let error){
+                    print(error)
+                }
+                return nil
+            }
             
-//            if actors.isEmpty {
-//                print(3)
+            if movies.isEmpty {
+                return nil
+            } else {
+                return movies
+            }
+        }, completion: completion)
+    }
+    
+//    func fetchMovieCredits(endpoint: Endpoint, completion: @escaping (APIResult<[Credit]>) -> Void) {
+//
+//        let request = endpoint.request
+//
+//        fetch(request: request, parse: { (json) -> [Credit]? in
+//            guard let movieCredits = json["cast"] as? [[String:AnyObject]], let movieId = json["id"] as? Int else {
+//                return nil
+//            }
+//
+//            let credits = movieCredits.flatMap { (credit) -> Credit? in
+//                do {
+//                    return try Credit(JSON: credit, movieId: movieId)
+//                } catch (let error){
+//                    print(error)
+//                }
+//                return nil
+//            }
+//
+//            if credits.isEmpty {
 //                return nil
 //            } else {
-//                return actors
+//                return credits
 //            }
-        }, completion: completion)
-    }
-    
-    //    Fecth leading actors of the movie
-    func fetchMovieDirector(movieId: String, completion: @escaping (APIResult<Crew>) -> Void) {
-        
-        let endpoint = MovieNightEndpoint.MovieCredits(id: movieId)
-        let request = endpoint.request
-        
-        fetch(request: request, parse: { (json) -> Crew? in
-            print(json["crew"])
-            guard let movieCredits = json["crew"] as? [[String:AnyObject]] else {
-                return nil
-            }
-            
-            let crews = movieCredits.compactMap { (crew) -> Crew? in
-                do {
-                    return try Crew(JSON: crew)
-                } catch (let error){
-                    print(error)
-                }
-                return nil
-            }
-            
-            var director = crews[0]
-            
-            for crew in crews {
-                if crew.job == "Director" {
-                    director = crew
-                }
-            }
-            
-            return director
-            
-//            if actors.count < 5 {
-//                return actors
-//            }
-//            else {
-//                return Array(actors.prefix(upTo: 5))
-//            }
-            
-        }, completion: completion)
-    }
-    
-    //    Fecth leading actors of the movie
-    func fetchVideo(movieId: String, completion: @escaping (APIResult<Video>) -> Void) {
-        
-        let endpoint = MovieNightEndpoint.MovieVideos(id: movieId)
-        let request = endpoint.request
-        
-        fetch(request: request, parse: { (json) -> Video? in
-            print(json["results"])
-            guard let results = json["results"] as? [[String:AnyObject]] else {
-                return nil
-            }
-            
-            let videos = results.compactMap { (video) -> Video? in
-                do {
-                    return try Video(JSON: video)
-                } catch (let error){
-                    print(error)
-                }
-                return nil
-            }
-            
-            return nil
-            
-//            if actors.count < 5 {
-//                return actors
-//            }
-//            else {
-//                return Array(actors.prefix(upTo: 5))
-//            }
-            
-        }, completion: completion)
-    }
+//        }, completion: completion)
+//    }
 
 }
