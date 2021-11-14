4705fa17c1ba073ef315676c611a71c1018318ce
Yalu Cai <yc468@duke.edu>
2021-11-14 17:06:01 -0500

    profile

---
+++ moivematcher/moivematcher/UserProfile/ProfileHeader.swift
@@ -0,0 +1,41 @@
+//
+//  ProfileHeader.swift
+//  moivematcher
+//
+//  Created by Yalu Cai on 11/14/21.
+//
+
+import SwiftUI
+
+struct ProfileHeader: View {
+    let gradient = Gradient(colors: [.blue, .purple])
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                
+                VStack {
+                SwiftUI.Image("clouds")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 200, height: 200)
+                        .clipShape(Circle())
+                        .clipped()
+//                        .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                        .padding(.top, 44)
+                    
+                    Text("Eddie Brock").font(.system(size: 20)).bold().foregroundColor(.white)
+                        .padding(.top, 12)
+                    
+                    Text("@venom").font(.system(size: 18)).foregroundColor(.white)
+                    .padding(.top, 4)
+                }
+                Spacer()
+            }
+            Spacer()
+        }
+        .background(LinearGradient(gradient: gradient, startPoint: .top, endPoint: .bottom))
+        .edgesIgnoringSafeArea(.all)
+    }
+}
---
+++ moivematcher/moivematcher/UserProfile/UserProfileViewController.swift
@@ -0,0 +1,39 @@
+//
+//  UserProfileViewController.swift
+//  moivematcher
+//
+//  Created by Yalu Cai on 11/14/21.
+//
+
+import UIKit
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ProfileHeader()
+    }
+}
+
+class UserProfileViewController: UIHostingController<ContentView> {
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder,rootView: ContentView());
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        let v : ContentView = ContentView()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
