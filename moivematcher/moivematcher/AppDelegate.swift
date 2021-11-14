c5a611d04e9948f3851332ec6a0f67f4fb86fa21
tong shen <ts325@duke.edu>
2021-11-14 14:10:18 -0500

    add model name

--- moivematcher/moivematcher/AppDelegate.swift
+++ moivematcher/moivematcher/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,7 +39,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
            application to it. This property is optional since there are legitimate
            error conditions that could cause the creation of the store to fail.
           */
-          let container = NSPersistentContainer(name: "coreDataDemo")
+          let container = NSPersistentContainer(name: "DataModel")
           container.loadPersistentStores(completionHandler: { (storeDescription, error) in
               if let error = error as NSError? {
                   // Replace this implementation with code to handle the error appropriately.
